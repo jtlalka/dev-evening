82256361070ac6d01085c60a8e33a78bd2eda811
Jakub Tlalka <jakub@tlalka.net>
2025-03-13 18:01:39 +0100

    Setup Wasm navigation

--- webPage.sh
+++ webPage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -r docs
-cp -r ../../ideas/dev-events/app/build/dist/wasmJs/productionExecutable/ docs
+cp -r ../../kmp/dev-events/app/build/dist/wasmJs/productionExecutable/ docs
 
 echo "WebPage update success :)"
